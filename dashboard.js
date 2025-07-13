fb5749e50371524e4a97c5417a3872eb59d09d3e
GiftsonYouthEcoLead <giftsonsituma79@gmail.com>
2025-07-13 16:54:15 +0300

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,29 @@
-import { auth } from './firebase.js';
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCecs-rafW9Wz2oc-elmp-YZplYpKhgLuM",
+  authDomain: "youthecoleadauth.firebaseapp.com",
+  projectId: "youthecoleadauth",
+  appId: "1:855744473089:web:e823c5426b4bfc2dbd0839"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const userEmail = document.getElementById("user-email");
+const logoutBtn = document.getElementById("logout-btn");
 
 onAuthStateChanged(auth, (user) => {
-  if (!user || !user.emailVerified) {
+  if (user) {
+    userEmail.textContent = `Logged in as: ${user.email}`;
+  } else {
     window.location.href = "login.html";
   }
 });
 
-window.logout = () => {
+logoutBtn.addEventListener("click", () => {
   signOut(auth).then(() => {
     window.location.href = "login.html";
   });
-};
+});
